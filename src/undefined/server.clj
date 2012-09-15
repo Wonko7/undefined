44d8aa50ff8dc002a7d0c12f820c2100a074fc58
Wonko7 <Wonko7@gmail.com>
2012-09-15 10:50:40 +0200

    first blood

---
+++ src/undefined/server.clj
@@ -0,0 +1,11 @@
+(ns undefined.server
+  (:require [noir.server :as server]))
+
+(server/load-views-ns 'undefined.views)
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (server/start port {:mode mode
+                        :ns 'undefined})))
+
---
+++ src/undefined/views/welcome.clj
@@ -0,0 +1,8 @@
+(ns undefined.views.welcome
+  (:require [undefined.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to undefined"]))
