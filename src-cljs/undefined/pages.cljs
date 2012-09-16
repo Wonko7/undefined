3530b38a4979336ea5bfe2f62751a16dcb97c1c5
Wonko7 <Wonko7@gmail.com>
2012-09-16 13:49:37 +0200

    add page load

--- src-cljs/undefined/pages.cljs
+++ src-cljs/undefined/pages.cljs
@@ -1,4 +1,5 @@
 (ns undef.pages
+  (:use [undef.init :only [add-init!]])
   (:require [fetch.remotes :as remotes]
             [enfocus.core :as ef])
   (:require-macros [fetch.macros :as fm]
@@ -8,3 +9,11 @@
   (fm/letrem [page (get-page href)]
     (em/at js/document
            [:#content] (em/content page))))
+
+(defn page-click [e]
+  (let [a (.-currentTarget e)]
+    (.preventDefault e)
+    (load-page (em/from a (em/get-attr :href)))))
+
+(add-init! #(em/at js/document
+                   [:#nav :a] (em/listen :click page-click)))
