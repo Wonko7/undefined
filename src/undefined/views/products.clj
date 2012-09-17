94acda100206600959ae293720a83ffc3c6bde36
Wonko7 <Wonko7@gmail.com>
2012-09-17 11:56:19 +0200

    remove useless spacing, I was thinking about latex, not html

--- src/undefined/views/products.clj
+++ src/undefined/views/products.clj
@@ -6,17 +6,9 @@
 (defn products-page [name product-id]
   (let [title       "Undefined's Products"
         fb-desc     (str "Budget Splitter is an application designed to help you share a budget on outings with friends. "
-                         "
-                         "
-                         "
-                         "
                          "Alice rented a car for the trip which cost her $75, Bob pays $50 for gas & Charlie $25 worth of pizzas. "
                          "Bob paid $15 for two cinema tickets and popcorn. Alice didn't go and won't be participating in that expense. "
                          "How to we equalise the expenses? Budget Splitter to the rescue!"
-                         "
-                         "
-                         "
-                         "
                          "Budget Splitter targets WebKit browsers: Iphone, Android, Chrome & Safari." ;; FIXME contract on the user agent.
                          )
         link        "/products/budget-splitter/index.html"
