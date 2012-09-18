317b7b837c732168721cf8e31c5052e47dad57f3
Wonko7 <Wonko7@gmail.com>
2012-09-18 18:29:44 +0200

    8000 port

--- src/undefined/server.clj
+++ src/undefined/server.clj
@@ -6,6 +6,6 @@
 ;; FIXME make different ports for test build and release builds.
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" "8000"))]
     (server/start port {:mode mode
                         :ns 'undefined})))
