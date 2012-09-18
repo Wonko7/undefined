72b5e7ffee0b32e41070985891eb8c7b7fff9097
Wonko7 <Wonko7@gmail.com>
2012-09-18 12:52:39 +0200

    sql korma for server

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
                  ;; clj:
                  [noir "1.3.0-beta8"]
                  [enlive "1.0.0"]
+                 [postgresql "8.4-702.jdbc4"] ;; FIXME: adapt for server/local testing
+                 [korma "0.3.0-beta9"]
                  ;; clj & cljs:
                  [fetch "0.1.0-alpha2"]
                  ;; cljs:
