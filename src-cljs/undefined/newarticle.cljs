409165a02b0bbf99c21090ae4af2ac0b4c8bf859
Landolphia <grimskunk@gmail.com>
2012-09-20 20:32:13 +0200

    First working version of article insertion

--- src-cljs/undefined/newarticle.cljs
+++ src-cljs/undefined/newarticle.cljs
@@ -5,13 +5,10 @@
   (:require-macros [fetch.macros :as fm]
      [enfocus.macros :as em]))
 
-(defn by-id [id] (.getElementById js/document id))
-
 (defn newarticlepage [href & [args]]
-<<<<<<< HEAD
-  (let [title (.-value (by-id "inp_title"));(em/from (by-id "#inp_title") (em/get-prop :value))
-        body  (em/from js/document [:textarea#txt_body] (em/get-prop :value))]
-    (js/console.log "called")
-    (em/at js/document [:#btn_add_article] (em/listen :click #(js/alert (str "Title: " title "\nBody: " body))))));#(fm/letrem [res (insert_article title body)] (js/alert res))))))
+  ;(js/console.log "called")
+  (em/at js/document [:#btn_add_article] (em/listen :click #(let [title (em/from (em/select ["#inp_title"]) (em/get-prop :value))
+                                                                  body  (em/from (em/select ["#txt_body"]) (em/get-prop :value))]
+                                                              (fm/letrem [res (insert_article title body)] (js/alert res))))))
 
 (add-page-init! "newarticle" newarticlepage)
