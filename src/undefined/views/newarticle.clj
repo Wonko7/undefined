8a7846ffa62ff189bff71720ea2ea4898325a0ef
Landolphia <grimskunk@gmail.com>
2012-09-20 18:50:23 +0200

    This test is brought to you by the chemical brothers (button test)

--- src/undefined/views/newarticle.clj
+++ src/undefined/views/newarticle.clj
@@ -1,15 +1,19 @@
 (ns undefined.views.newarticle
   (:require [net.cgrand.enlive-html :as html])
   (:use [undefined.views.common :only [add-page-init! page newarticle]]
-        [undefined.sql :only [select_articles insert_article insert_article2
-                              select_tags tags_by_article tags_by_label
-                              select_categories categories_by_article
-                              select_authors authors_by_article]]
-        [noir.fetch.remotes]))
+     [undefined.sql :only [select_articles insert_article insert_article2
+                           select_tags tags_by_article tags_by_label
+                           select_categories categories_by_article
+                           select_authors authors_by_article]]
+     [undefined.views.common :only [base page newarticle metadata]]
+     [noir.fetch.remotes]
+     [noir.core :only [defpage pre-route]]))
 
-(defn new-article-page []
-  (page "Add a new post" 
+(defremote get-text [] ("test"))
+
+(defpage "/newarticle" []
+ (base (page "Add a new post" 
         (newarticle (select_authors) (select_categories))
-        {:metadata {:data-init-page "newpage"}}))
+        {:metadata {:data-init-page "newpage"}})))
 
-(add-page-init! "newarticle" new-article-page)
+;(add-page-init! "newarticle" new-article-page)
