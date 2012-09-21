f72c6fd369a2530709fd83ad4334202ca8b8ba9e
Wonko7 <Wonko7@gmail.com>
2012-09-21 17:24:36 +0200

    add news blogs pages draft, debug chrome history

--- src-cljs/undefined/pages.cljs
+++ src-cljs/undefined/pages.cljs
@@ -41,9 +41,7 @@
     (init-page)))
 
 
-(declare page-click)
-
-(def history (hist/history (fn [{:keys [token navigation?]}]
+(def history (hist/history (fn [{:keys [type token navigation?]}]
                              (when navigation?
                                (page-click token nil :no-hist)))))
 
