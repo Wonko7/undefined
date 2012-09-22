642718d2ed3764e4b0809c36044d952ae9ca424d
Landolphia <grimskunk@gmail.com>
2012-09-22 13:00:17 +0200

    Added auto focus on login/add article forms

--- src-cljs/undefined/newarticle.cljs
+++ src-cljs/undefined/newarticle.cljs
@@ -6,15 +6,17 @@
      [enfocus.macros :as em]))
 
 (defn newarticlepage [href & [args]]
-  (em/at js/document [:#btn_add_article] (em/listen :click #(let [title      (em/from (em/select ["#inp_title"]) (em/get-prop :value))
-                                                                  body       (em/from (em/select ["#txt_body"]) (em/get-prop :value))
-                                                                  tags       (em/from (em/select ["#inp_tags"]) (em/get-prop :value))
-                                                                  authors    (zipmap
-                                                                               (em/from (em/select [".cbx_auth"]) (em/get-prop :value))
-                                                                               (em/from (em/select [".cbx_auth"]) (em/get-prop :checked)))
-                                                                  categories (zipmap
-                                                                               (em/from (em/select [".cbx_cat"]) (em/get-prop :value))
-                                                                               (em/from (em/select [".cbx_cat"]) (em/get-prop :checked)))]
-                                                              (fm/letrem [res (insert_article title body tags authors categories)] (js/console.log (str "Inserted article: " res)))))))
+  (em/at js/document
+      [:#inp_title] (em/focus)
+      [:#btn_add_article] (em/listen :click #(let [title      (em/from (em/select ["#inp_title"]) (em/get-prop :value))
+                                                   body       (em/from (em/select ["#txt_body"]) (em/get-prop :value))
+                                                   tags       (em/from (em/select ["#inp_tags"]) (em/get-prop :value))
+                                                   authors    (zipmap
+                                                                (em/from (em/select [".cbx_auth"]) (em/get-prop :value))
+                                                                (em/from (em/select [".cbx_auth"]) (em/get-prop :checked)))
+                                                   categories (zipmap
+                                                                (em/from (em/select [".cbx_cat"]) (em/get-prop :value))
+                                                                (em/from (em/select [".cbx_cat"]) (em/get-prop :checked)))]
+                                               (fm/letrem [res (insert_article title body tags authors categories)] (js/console.log (str "Inserted article: " res)))))))
 
 (add-page-init! "newarticle" newarticlepage)
