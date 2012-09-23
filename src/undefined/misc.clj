e470fa9d05c9d54ac58fc2332081b7ab6af7b522
Wonko7 <Wonko7@gmail.com>
2012-09-23 17:42:36 +0200

    better date format

--- src/undefined/misc.clj
+++ src/undefined/misc.clj
@@ -3,8 +3,7 @@
         [clj-time.coerce :as time-conv]
         [clj-time.format :as time-format]))
 
-
-(def date-format (time-format/formatters :rfc822))
+(def date-format (time-format/formatter "EEE, dd MMM yyyy HH:mm"))
 
 (defn format-date [sql-date]
   (time-format/unparse date-format (time-conv/from-sql-date sql-date)))
