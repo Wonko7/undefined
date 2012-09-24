a9f51aa85b214998331ce77fe0eb4c0907102dae
Landolphia <grimskunk@gmail.com>
2012-09-24 22:20:05 +0200

    Delete the right article

--- src-cljs/undefined/news.cljs
+++ src-cljs/undefined/news.cljs
@@ -1,14 +1,16 @@
-=(ns undef.news
- (:use [undef.pages :only [add-page-init!]])
- (:require [fetch.remotes :as remotes]
-  [enfocus.core :as ef])
- (:require-macros [fetch.macros :as fm]
-  [enfocus.macros :as em]))
+(ns undef.news
+		(:use [undef.pages :only [add-page-init! page-click]])
+		(:require [fetch.remotes :as remotes]
+		 [enfocus.core :as ef])
+		(:require-macros [fetch.macros :as fm]
+		 [enfocus.macros :as em]))
 
+;TODO remove page click and remove the div instead
 (defn newspage [href & [args]]
- (em/at js/document
-  [:#btn_del] (em/listen :click #(if(js/confirm "This will PERMANENTLY erase the article from the database.")
-		  (fm/letrem [res (delete_article 1)] (page-click "news" nil)))
-  [:#btn_upd] (em/listen :click #(js/alert "Update"))))
+ (let [uid (em/from (em/select ["#btn_del"]) (em/get-prop :value))]
+  (em/at js/document
+   [:#btn_del] (em/listen :click #(if(js/confirm "This will PERMANENTLY erase the article from the database.")
+		   (fm/letrem [res (delete_article uid)] (page-click "news" nil))))
+	   [:#btn_upd] (em/listen :click #(js/alert "Update")))))
 
-(add-page-init! "news" newspage)
+ (add-page-init! "news" newspage)
--- src/undefined/views/common.clj
+++ src/undefined/views/common.clj
@@ -23,8 +23,8 @@
   [:.categories]       (html/content categories)
   [:.authors]          (html/content authors)
   [:.admin]            (html/append (if (is-admin?)
-                                      [{:tag :button :attrs {:id "btn_upd"} :content "Edit"}
-                                       {:tag :button :attrs {:id "btn_del"} :content "Delete"}])))
+                                      [{:tag :button :attrs {:id "btn_upd" :value (str uid)} :content "Edit"}
+                                       {:tag :button :attrs {:id "btn_del" :value (str uid)} :content "Delete"}])))
                                      ; [{:tag :a :attrs {:data-href "edit-link" :data-args (str uid) } :content "Edit"}
                                      ;  {:tag :span :content " "} ;; just for spacing
                                      ;  {:tag :a :attrs {:data-href "delete-link" :data-args (str uid) } :content "Delete"}])))
