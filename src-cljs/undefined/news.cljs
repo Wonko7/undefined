56519448c513cbb009032416b9d982fed0b2b356
Wonko7 <Wonko7@gmail.com>
2012-09-27 12:52:40 +0200

    better enfocus on news page / edit blog article

--- src-cljs/undefined/news.cljs
+++ src-cljs/undefined/news.cljs
@@ -7,27 +7,34 @@
 
 (defn newspage [href & [args]]
   (em/at js/document
-      [:.btn_del] (em/remove-listener :click)
-      [:.btn_upd] (em/remove-listener :click)
-      [:.btn_del] (em/listen :click (fn [e]
-                                      (let [uid (em/from (.-currentTarget e) (em/get-attr :value))]
-                                        (if (js/confirm (str "This will PERMANENTLY erase the article #" uid " from the database."))
-                                          (fm/letrem [res (delete_article_rem uid)] (em/at js/document [(str ":#article_" uid)] (em/substitute "")))))))
-      [:.btn_upd] (em/listen :click (fn [e]
-                                      (let [uid    (int (em/from (.-currentTarget e) (em/get-attr :value)))
-                                            sel    (str ":#article_" uid)]
-                                        (fm/letrem [div (get-page "news-update-article-div" uid)]
-                                            (em/at js/document
-                                                [sel] (em/html-content div)
-                                                [:form] (em/listen :submit
-                                                            (fn [e]
-                                                              (.preventDefault e)
-                                                              (let [newtitle (em/from (em/select [(str sel " .inp_title" )])  (em/get-prop :value))
-                                                                    newbody  (em/from (em/select [(str sel " .txt_body" )])   (em/get-prop :value))
-                                                                    newtags  (em/from (em/select [(str sel " .inp_tags" )])   (em/get-prop :value))]
-                                                                (fm/letrem [res (update_article_rem uid newtitle newbody newtags)
-                                                                            div (get-page "news-refresh-article-div" uid)]
-                                                                    (em/at js/document [sel] (em/substitute div))
-                                                                    (newspage href args))))))))))));FIXME only refresh the new buttons?
+         [:.btn_del] (em/do->
+                       (em/remove-listener :click)
+                       (em/listen :click (fn [e]
+                                           (let [uid (em/from (.-currentTarget e) (em/get-attr :value))]
+                                             (when (js/confirm (str "This will PERMANENTLY erase the article #" uid " from the database."))
+                                               (fm/letrem [res (delete_article_rem uid)]
+                                                 (em/at js/document [(str ":#article_" uid)] (em/substitute ""))))))))
+         [:.btn_upd] (em/do->
+                       (em/remove-listener :click)
+                       (em/listen :click (fn [e]
+                                           (let [uid    (int (em/from (.-currentTarget e) (em/get-attr :value)))
+                                                 sel    (str ":#article_" uid)]
+                                             (fm/letrem [div (get-page "news-update-article-div" uid)]
+                                               (em/at js/document
+                                                      [sel] (em/html-content div)
+                                                      [:form] (em/listen :submit
+                                                                         (fn [e]
+                                                                           (.preventDefault e)
+                                                                           (let [article (em/from js/document
+                                                                                                  :title [(str sel " .inp_title" )] (em/get-prop :value)
+                                                                                                  :body  [(str sel " .txt_body" )]  (em/get-prop :value)
+                                                                                                  :tags  [(str sel " .inp_tags" )]  (em/get-prop :value))]
+                                                                             (fm/letrem [res (update_article_rem uid
+                                                                                                                 (:title article)
+                                                                                                                 (:body article)
+                                                                                                                 (:tags article))
+                                                                                         div (get-page "news-refresh-article-div" uid)]
+                                                                               (em/at js/document [sel] (em/substitute div))
+                                                                               (newspage href args)))))))))))));FIXME only refresh the new buttons?
 
 (add-page-init! "news" newspage)
