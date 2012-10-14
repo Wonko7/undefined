1fa38731a46e44a96af67b7c974ba24a4cf81e77
Landolphia <landolphia@undefined.re>
2012-10-14 23:16:44 +0200

    db.sql should be up to date and populate the db with enough data to test

--- DB.sql
+++ DB.sql
@@ -1,4 +1,8 @@
-DROP TABLE IF EXISTS article_categories, article_tags, article_authors, author_roles, articles, categories, tags, authors, projects, roles, comments;
+-- psql undefined <
+-- I suggest testing on a new temp db before, and changing the db name in sql.clj
+-- to create a new DB: CREATE DATABASE name;
+
+DROP TABLE IF EXISTS article_authors, article_categories, article_tags, articles, author_roles, authors, categories, comments, projects, reset_links, roles, tags, temp_authors;
 
 CREATE TABLE articles (uid SERIAL PRIMARY KEY, title TEXT NOT NULL, body TEXT NOT NULL, birth TIMESTAMP (0) DEFAULT CURRENT_TIMESTAMP);
 
@@ -13,9 +17,6 @@ CREATE TABLE temp_authors (uid SERIAL PRIMARY KEY, username TEXT UNIQUE NOT NULL
 CREATE UNIQUE INDEX username_temp_lower_id ON temp_authors (lower(username));
 CREATE UNIQUE INDEX email_temp_lower_id ON temp_authors (lower(email));
 
-CREATE TABLE reset_links (uid SERIAL PRIMARY KEY, userid INTEGER references authors(uid) ON DELETE CASCADE, birth TIMESTAMP (0) DEFAULT CURRENT_TIMESTAMP, resetlink TEXT NOT NULL);
-
-
 CREATE TABLE roles (uid SERIAL PRIMARY KEY, label TEXT UNIQUE NOT NULL);
 
 CREATE TABLE article_categories (artid INTEGER references articles(uid) ON DELETE CASCADE, catid INTEGER references categories(uid) ON DELETE CASCADE, PRIMARY KEY (artid, catid));
@@ -27,6 +28,8 @@ CREATE TABLE author_roles (authid INTEGER references authors(uid) ON DELETE CASC
 CREATE TABLE projects (uid SERIAL PRIMARY KEY, title TEXT NOT NULL, description TEXT NOT NULL, link TEXT NOT NULL, screenshot TEXT NOT NULL, pin INTEGER UNIQUE NOT NULL);
 
 CREATE TABLE comments (uid SERIAL PRIMARY KEY, content TEXT NOT NULL, artid INTEGER references articles(uid) ON DELETE CASCADE, authid INTEGER references authors(uid), birth TIMESTAMP (0) DEFAULT CURRENT_TIMESTAMP, edit TIMESTAMP (0) DEFAULT NULL);
+
+CREATE TABLE reset_links (uid SERIAL PRIMARY KEY, userid INTEGER references authors(uid) ON DELETE CASCADE, birth TIMESTAMP (0) DEFAULT CURRENT_TIMESTAMP, resetlink TEXT NOT NULL);
 
 
 INSERT INTO categories (label) VALUES ('blog'), ('news');
@@ -40,4 +43,86 @@ INSERT INTO roles (label) VALUES ('peon'), ('admin'), ('contributor');
 
 INSERT INTO author_roles VALUES (1, 2), (2, 2);
 
-GRANT ALL PRIVILEGES ON articles, comments, tags, authors, categories, article_tags, article_categories, article_authors, projects, roles, author_roles, tags_uid_seq, articles_uid_seq, comments_uid_seq TO web;
+-- Populates DB with test data which ressembles the current state of the site + data for unreleased features
+
+INSERT INTO articles VALUES (2, 'Undefined is up!', '<p>Undefined now has a website. We''ll be showing our
+<a href="projects">projects</a>, and keeping the technical blog & news
+section up to date. We have feeds for your favourite news aggragator; 
+<a data-ext="true" href="http://undefined.re/news-feed">News Feed</a> and 
+<a data-ext="true" href="http://undefined.re/blog-feed">Blog Feed</a>.
+</p>
+
+<p>The website''s <a href="https://github.com/defined/undefined/" data-ext="true">source code</a>
+is on github, check it out if you''re into clojure.</p>', '2012-10-07 18:03:56');
+INSERT INTO articles VALUES (1, 'Vim sniper', '<p>Vim''s cursorline & cursorcolumn are widely used and known and loved settings.
+I never got used to them, they always end up annoying me; I like being able to
+spot the cursor that easily, but while reading code I find it distracting.</p>
+
+<p>So this is what I ended up using:</p>
+
+<div class="code">set updatetime=2000
+
+function! MySetCursor()
+	set cursorline
+	set cursorcolumn
+endfunction
+function! MyUnSetCursor()
+	set nocursorline
+	set nocursorcolumn
+endfunction
+
+au! CursorHold * call MyUnSetCursor()
+au! CursorMoved * call MySetCursor()
+au! CursorMovedI * call MyUnSetCursor()</div>
+
+<p>It shows the cursor line & column while moving around in a buffer, hides it
+after an idle time of 2 seconds, and hides it in input mode (because if I''m
+writing in a buffer, hopefully I already know where I am...).</p>
+', '2012-10-07 18:02:03');
+INSERT INTO articles VALUES (3, 'McGyver''s null modem', '<p>What do you do when you want to connect two female D9 connectors and
+the shops are closed when you realise that what you don''t have a null modem?
+You badly want to play with an embedded device...
+</p>
+
+<p>If you feel in a McGyver kind of mood, you can make yourself one with a few
+wires. I used cigarette filters to hold the wires in place (my first idea was
+cork but couldn''t find any).<br>
+<center><img src="img/null-modem.png" /></center>
+</p>
+
+<p>More information on RS232 serial null modem cable wiring <a href="http://www.lammertbies.nl/comm/info/RS-232_null_modem.html">here</a>.
+</p>', '2012-10-09 16:15:24');
+
+INSERT INTO tags VALUES (1, 'vim');
+INSERT INTO tags VALUES (2, 'annoucement');
+INSERT INTO tags VALUES (3, 'undefined');
+INSERT INTO tags VALUES (4, 'hacks');
+
+INSERT INTO article_authors VALUES (2, 2);
+INSERT INTO article_authors VALUES (2, 1);
+INSERT INTO article_authors VALUES (1, 2);
+INSERT INTO article_authors VALUES (3, 2);
+
+INSERT INTO article_categories VALUES (2, 2);
+INSERT INTO article_categories VALUES (1, 1);
+INSERT INTO article_categories VALUES (3, 1);
+
+INSERT INTO article_tags VALUES (2, 3);
+INSERT INTO article_tags VALUES (1, 1);
+INSERT INTO article_tags VALUES (3, 4);
+
+INSERT INTO comments (content, artid, authid, birth) VALUES ('This does not mean my bath is red', 1, 1, '2012-10-07 18:03:56'),
+ ('What do you mean?', 1, 2, '2012-10-07 18:17:23'),
+ ('I mean it is not red, why is that so hard to understand?', 1, 1, '2012-10-07 20:34:10'),
+ ('I apologize for my daughter, she used my account while I was taking a red bath. Please disregard the previous comments she made on my behalf', 1, 1, '2012-10-07 21:51:57'),
+ ('You took a two day long bath? I guess the fact that it was red does not seem so weird anymore', 1, 2, '2012-10-08 09:42:15'),
+ ('What an interesting post, I wish I could marry it', 2, 2, '2012-11-03 12:11:21'),
+ ('The way things are going I will not be able to come home because of the cheesecake fairy', 3, 2, '2012-12-07 17:17:17'),
+ ('I guess now you see why we need a better way to generate content randomly.', 3, 2, '2012-12-09 18:17:23');
+
+-- template to insert a reset_link, use the sql.clj fn before trying that?
+-- INSERT INTO reset_links (userid, resetlink) VALUES (1, 'hashyhashash');
+-- same for temp_authors
+-- INSERT INTO temp_authors (username, email, password, salt, activation) VALUES ('John', 'john@doe.re', 'hashedhashofhashyhashhash', 'uselessfornow', 'hashyhashofhashedhashwithhashontheside');
+
+GRANT ALL PRIVILEGES ON article_authors, article_categories, article_tags, articles, author_roles, authors, categories, comments, projects, reset_links, roles, tags, temp_authors, tags_uid_seq, articles_uid_seq, comments_uid_seq TO web;
