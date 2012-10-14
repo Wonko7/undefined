4dc4deeb96bc4f26cf8baf0efe2646ece3aaf881
Landolphia <landolphia@undefined.re>
2012-10-14 14:24:58 +0200

    working on login.cljs

--- src-cljs/undefined/login.cljs
+++ src-cljs/undefined/login.cljs
@@ -2,25 +2,32 @@
   (:use [undef.pages :only [add-page-init! page-click]]
         [undef.misc :only [show-admin-stuff]])
   (:require [fetch.remotes :as remotes]
-     [enfocus.core :as ef])
+            [enfocus.core :as ef])
   (:require-macros [fetch.macros :as fm]
-     [enfocus.macros :as em]))
+                   [enfocus.macros :as em]))
 
 (defn login-page [href & [args]]
-      (em/at js/document
-             [:#page :a.logout] (em/listen :click (fn [e]
-                                                    (.preventDefault e)
-                                                    (fm/letrem [res (auth-logout)]
-                                                      (page-click "news" nil))))
-             [:#inp_usr] (em/focus)
-             [:form] (em/listen :submit (fn [e]
-                                          (.preventDefault e)
-                                          (let [id (em/from js/document
-                                                            :user [:form :input.user] (em/get-prop :value)
-                                                            :pass [:form :input.pass] (em/get-prop :value))]
-                                            (fm/letrem [[user roles] (auth-login id)]
-                                              (if user
-                                                (page-click "news" nil)
-                                                (js/alert (str "log in failed. ")))))))))
+  (let [login #(em/at js/document
+                            [:#page :a.logout] (em/listen :click (fn [e]
+                                                                   (.preventDefault e)
+                                                                   (fm/letrem [res (auth-logout)]
+                                                                     (page-click "news" nil))))
+                            [:#inp_usr] (em/focus)
+                            [:form] (em/listen :submit (fn [e]
+                                                         (.preventDefault e)
+                                                         (let [id (em/from js/document
+                                                                           :user [:form :input.user] (em/get-prop :value)
+                                                                           :pass [:form :input.pass] (em/get-prop :value))]
+                                                           (fm/letrem [[user roles] (auth-login id)]
+                                                             (if user
+                                                               (page-click "news" nil)
+                                                               (js/alert (str "log in failed. "))))))))
+        profile #(em/at js/document
+                              [:#page :a.logout] (em/listen :click (fn [e]
+                                                                     (.preventDefault e)
+                                                                     (fm/letrem [res (auth-logout)]
+                                                                       (page-click "news" nil)))))]
+    (fm/letrem [[user roles] (get-user)]
+      (js/console.log (str "test" user roles)))))
 
 (add-page-init! "login" login-page)
