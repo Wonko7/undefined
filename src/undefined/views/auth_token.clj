474d2cfc23da7667c8c1fd1c21f51dad14343a82
Wonko7 <Wonko@undefined.re>
2012-10-14 22:15:10 +0200

    add redirect hack

--- src/undefined/views/auth_token.clj
+++ src/undefined/views/auth_token.clj
@@ -12,5 +12,6 @@
 (add-page-init! "validate" #(page "Account Validation"
                                   {:tag :div :attrs {:class "whole-article"}
                                    :content [{:tag :div :attrs {:class "article"}
-                                              :content [(activate_user %3)]}]})
+                                              :content [(activate_user %3)]}]}
+                                  {:metadata {:data-init-page "404"}})
                 1)
