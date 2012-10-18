6324045f88bddf3009d4573af7b76a5a32ecaa57
Wonko7 <Wonko@undefined.re>
2012-10-18 12:17:12 +0200

    remove js comment size test. remove get count from edit comm. harmonise form ok/cancel button order.

--- src-cljs/undefined/news.cljs
+++ src-cljs/undefined/news.cljs
@@ -51,8 +51,7 @@
                       (js/alert "Your comment is empty...")
                       (fm/letrem [res (update_comment_rem uid comment)
                                   div (get-page "refresh-comment-div" uid)]
-                        (animate-replace div)
-                        (update-comment-count-li uid))))))))
+                        (animate-replace div))))))))
 
           (delete-button [type]
             (fn [e]
@@ -85,8 +84,6 @@
                                              :body [:textarea]         (em/get-prop :value))]
               (if (re-find #"^\s*$" body)
                 (js/alert "Your comment is empty.")
-                (if (> (.length body) 10000)
-                  (js/alert "Your comment cannot be more than 10000 characters long.")
                   (do
                     (fm/letrem [res (insert_comment_rem id body)
                                 div (get-page "fetch-comment-div" res)]
@@ -98,7 +95,7 @@
                                                                            (ef/chainable-standard #(em/at %
                                                                                                           [:.btn_del_c_and_a] (em/listen :click (delete-button :comment))
                                                                                                           [:.btn_upd_c_and_a] (em/listen :click (update-button :comment))))
-                                                                           (restore-height 200)))))))))]
+                                                                           (restore-height 200))))))))]
 
     (em/at js/document
       [:.btn_del]         (em/listen :click (delete-button :article))
