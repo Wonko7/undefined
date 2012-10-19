93aa9cfacce63c30561f17ddf0a0153be21c5280
Wonko7 <Wonko@undefined.re>
2012-10-19 14:50:38 +0200

    add html to emails

--- src/undefined/misc.clj
+++ src/undefined/misc.clj
@@ -34,20 +34,27 @@
                  :reset       "Password reset request, undefined.re"
                  :change      "Email update request, undefined.re")
         body    (case typ
-                  :activation  (str "Thank you for registering an account at undefined.re,\n\nfollow the link below to activate your account and start posting comments\n"
-                                    "http://undefined.re/activate/" token 
-                                    "\nThis link will expire in 24 hours."
-                                    "\n\nRegards,\n\n~The Undefined team.")
-                  :reset       (str "If you haven't requested your password to be reset please ignore this email.\n\n"
-                                    "To reset your password follow the link below\n"
-                                    "http://undefined.re/reset/" token
-                                    "\nThis link will expire in 24 hours."
-                                    "\n\nRegards,\n\n~The Undefined team.")
-                  :change      (str "If you haven't requested your email to be changed please ignore this email.\n\n"
-                                    "To confirm your new email address follow the link below\n"
-                                    "http://undefined.re/update/" token
-                                    "\nThis link will expire in 24 hours."
-                                    "\n\nRegards,\n\n~The Undefined team."))
+                  :activation  (str "<html><head></head><body>"
+                                    "Thank you for registering an account at undefined.re,<br><br>"
+                                    "follow the link below to activate your account and start posting comments:<br>"
+                                    "<a href=\"http://undefined.re/activate/" token  "\">Activate Accout</a>"
+                                    "<br>This link will expire in 24 hours."
+                                    "<br><br>Regards,<br><br>~The Undefined team."
+                                    "</body></html>")
+                  :reset       (str "<html><head></head><body>"
+                                    "If you haven't requested your password to be reset please ignore this email.<br><br>"
+                                    "To reset your password follow the link below<br>"
+                                    "<a href=\"http://undefined.re/reset/" token  "\">Confirm Password Reset</a>"
+                                    "<br>This link will expire in 24 hours."
+                                    "<br><br>Regards,<br><br>~The Undefined team."
+                                    "</body></html>")
+                  :change      (str "<html><head></head><body>"
+                                    "If you haven't requested your email to be changed please ignore this email.<br><br>"
+                                    "To confirm your new email address follow the link below<br>"
+                                    "<a href=\"http://undefined.re/update/" token  "\">Confirm Email Update</a>"
+                                    "<br>This link will expire in 24 hours."
+                                    "<br><br>Regards,<br><br>~The Undefined team."
+                                    "</body></html>"))
         smtp_pass (:smtp_pass (get-config))]
     (ps/send-message ^{:host  "smtp.gmail.com"
                        :user  "landolphia@undefined.re"
@@ -56,4 +63,5 @@
                      {:from     "defined@undefined.re"
                       :to       email
                       :subject  subject
-                      :body     body})))
+                      :body     [{:type "text/html"
+                                  :content body}]})))
